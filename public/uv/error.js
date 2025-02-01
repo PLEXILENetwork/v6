325f8477e0ad6d1bb77d46c42a0ce4c2462b729f
willo <153154914+willoo0@users.noreply.github.com>
2025-02-01 03:30:16 +0000

    Update 🚀

--- public/uv/error.js
+++ public/uv/error.js
@@ -2,27 +2,11 @@
 const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 const registerButton = document.getElementById("register-proxy");
-const page = window.location.pathname;
-
-let historyStack = JSON.parse(sessionStorage.getItem("historyStack")) || [];
-historyStack.push(page);
-if (historyStack.length > 2) historyStack.shift();
-sessionStorage.setItem("historyStack", JSON.stringify(historyStack));
-
-if (sessionStorage.getItem("returnToPage")) {
-  const returnTo = sessionStorage.getItem("returnToPage");
-  sessionStorage.removeItem("returnToPage"); 
-  window.location.href = returnTo; 
-}
+const page = window.location.pathname
 
 if (registerButton) {
   registerButton.addEventListener("click", async () => {
     try {
-      
-      const historyStack = JSON.parse(sessionStorage.getItem("historyStack")) || [];
-      const returnToPage = historyStack.length > 1 ? historyStack[0] : page;
-      sessionStorage.setItem("returnToPage", returnToPage); 
-
       await registerSW();
       location.reload();
     } catch (err) {
