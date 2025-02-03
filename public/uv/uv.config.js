7b8ee3e7e28219b64fdf05b1385c76da6fb01560
#willo <153154914+willoo0@users.noreply.github.com>
2025-02-03 14:13:06 -0800

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/pa/',
-    bare: 'https://bare.benrogo.net/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
